2c60b4ba6c56373ff99869989abf31606026843c
nailoc23 <nailoc23@naver.com>
2024-09-04 17:55:09 +0900

    관리자 - 로그인 화면 관리자 - 메인 화면

--- src/main/java/com/touzone/stsb/controller/ManagerController.java
+++ src/main/java/com/touzone/stsb/controller/ManagerController.java
@@ -1,16 +1,14 @@
 package com.touzone.stsb.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class ManagerController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
+    @GetMapping("/admin/login")
+    public String admin_login() {
+        return "admin/login";
     }
-
-
 }
--- src/main/java/com/touzone/stsb/controller/SaleController.java
+++ src/main/java/com/touzone/stsb/controller/SaleController.java
@@ -2,15 +2,12 @@ package com.touzone.stsb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class SaleController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
+    @GetMapping("/admin/dash")
+    public String dash() {
+        return "admin/dash";
     }
-
-
 }
